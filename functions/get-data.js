fa5d3e94a40bc374da29cada97fff17a39104a5d
Luke Batchelor <lbatchelor@atlassian.com>
2020-01-08 20:42:50 +1100

    Core functionality all done

--- functions/get-data.js
+++ functions/get-data.js
@@ -1,18 +1,23 @@
 const https = require('https');
 
-const { donorbox_user, donorbox_key, campaign_id} = process.env;
+const { donorbox_user, donorbox_key, campaign_id, default_pages_to_fetch } = process.env;
 
-function getRequest(url) {
+const apiBaseUrl = 'https://donorbox.org/api/v1';
+
+// Creating our own getRequest function so we don't need to import anything except default
+// node `https`
+function getRequest(url, extraParams = '') {
   const auth = Buffer.from(`${donorbox_user}:${donorbox_key}`).toString('base64');
   const options = {
     headers: {
       Authorization: `Basic ${auth}`,
     },
   };
+  const queryParams = `?campaign_id=${campaign_id}&per_page=100&order=desc` + extraParams;
 
   return new Promise((resolve, reject) => {
     let data = '';
-    const req = https.get(url, options, resp => {
+    const req = https.get(url + queryParams, options, resp => {
       resp.on('data', chunk => (data += chunk));
       resp.on('end', () => resolve(JSON.parse(data)));
     });
@@ -21,14 +26,53 @@ function getRequest(url) {
   });
 }
 
+async function getCampaignData() {
+  const url = apiBaseUrl + '/campaigns';
+  const resp = await getRequest(url);
+  const campaign = resp.find(c => `${c.id}` === campaign_id);
+
+  return campaign;
+}
+
+
+async function getDonationData() {
+  const initialPages = Number(default_pages_to_fetch);
+  const promises = [];
+  const url = apiBaseUrl + '/donations';
+
+  // Create an array of promises for all the pages so we can run all the requests in parallel
+  for (let i = 1; i <= initialPages; i++) {
+    const promise = getRequest(url, `&page=${i}`).then(donations => {
+      // Strip out all the sensitive info we don't need
+      return donations.map(donation => {
+        const cleanedData = {
+          designation: donation.designation,
+          comment: donation.comment,
+          fallbackName: donation.donor.first_name,
+          amount: Number(donation.amount)
+        }
+        if (donation.questions[0]) {
+          cleanedData.alias = donation.questions[0].answer
+        }
+        return cleanedData
+      })
+    })
+    promises.push(promise);
+  }
+
+  const allData = await Promise.all(promises);
+  // todo: check that all the data is here and we don't need to fetch more
+
+  return allData.flat();
+}
+
 exports.handler = async function(event, context) {
-  const apiBaseUrl = 'https://donorbox.org/api/v1/donations';
-  const resp = await getRequest(apiBaseUrl);
+  const [campaignData, donationData ] = await Promise.all([getCampaignData(), getDonationData()]);
+  const lastDonation = donationData[0];
+  const highestDonation = donationData.reduce((prev, cur) => cur.amount > prev.amount ? cur : prev, { amount: 0 });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(resp)
+    body: JSON.stringify({ campaignData, donationData, lastDonation, highestDonation })
   }
-
-
 }
--- site/fetch-data.js
+++ site/fetch-data.js
@@ -3,20 +3,50 @@ function displayLatestDonation() {
   const latestDonationElem = document.querySelector('.recent-donator');
   latestDonationElem.style.opacity = 1;
   setInterval(() => {
-    latestDonationElem.style.opacity = 0.01;
+    latestDonationElem.style.opacity = 0;
   }, 5000);
 }
 
-function fetchData() {
+function updateData() {
+  // from https://stackoverflow.com/a/14428340
+  const toMoneyStr = amt => '$' + amt.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  fetch('/.netlify/functions/get-data')
+    .then(r => r.json())
+    .then(data => {
+      const { campaignData, donationData, lastDonation, highestDonation} = data;
+      const totalRaised = Number(campaignData.total_raised);
+      const goalAmt = Number(campaignData.goal_amt);
+      const raisedPercent = Math.min(totalRaised / goalAmt, 1) * 100;
+      const lastDonationName = lastDonation.alias || lastDonation.fallbackName || '';
+      const lastDonationAmt = Number(lastDonation.amount);
+      const lastDonationComment = lastDonation.comment || '';
+      const highestDonationName = highestDonation.alias || highestDonation.fallbackName || '';
+      const highestDonationAmt = Number(highestDonation.amount);
+      const recentDonatorStr = `${lastDonationName} donated ${toMoneyStr(lastDonationAmt)}`;
 
+      const raisedEl = document.querySelector('.raised-box div:nth-child(2)');
+      const raisedBarEl = document.querySelector('.raised-bar .bar');
+      const goalEl = document.querySelector('.goal-box div:nth-child(2)');
+      const lastDonationEl = document.querySelector('.last-donation-box div:nth-child(2)');
+      const highestDonationEl = document.querySelector('.highest-donation-box div:nth-child(2)');
+      const recentDonatorNameEl = document.querySelector('.recent-donator .donator');
+      const recentDonatorCommentEl = document.querySelector('.recent-donator .donation-message');
+
+      raisedEl.innerText = toMoneyStr(totalRaised);
+      goalEl.innerText = toMoneyStr(goalAmt);
+      raisedBarEl.style.width = raisedPercent + '%';
+      lastDonationEl.innerText = lastDonationName + ' ' + toMoneyStr(lastDonationAmt);
+      highestDonationEl.innerText = highestDonationName + ' ' + toMoneyStr(highestDonationAmt);
+      recentDonatorNameEl.innerText = recentDonatorStr;
+      recentDonatorCommentEl.innerText = lastDonationComment;
+    });
 }
 
 document.body.addEventListener('keydown', (e) => {
   if (e.keyCode === 32 || e.keyCode === 65) {
     displayLatestDonation();
   }
-})
+});
 
-fetch('/.netlify/functions/get-data').then(r => r.json()).then(r => {
-  console.log(r)
-})
+updateData()
+window.setInterval(updateData, 30 * 1000); // update every thirty seconds
